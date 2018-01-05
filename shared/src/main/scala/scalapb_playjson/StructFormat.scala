69726f91316d2ab274e8447b943d34a38210a05a
xuwei-k <6b656e6a69@gmail.com>
2018-01-05 09:55:49 +0900

    avoid mapValues

--- shared/src/main/scala/scalapb_playjson/StructFormat.scala
+++ shared/src/main/scala/scalapb_playjson/StructFormat.scala
@@ -35,7 +35,7 @@ object StructFormat {
   }
 
   def structWriter(v: struct.Struct): JsValue =
-    JsObject(v.fields.mapValues(structValueWriter))
+    JsObject(v.fields.map{ case (x, y) => (x, structValueWriter(y))})
 
   def listValueParser(v: JsValue): struct.ListValue = v match {
     case JsArray(elems) =>
