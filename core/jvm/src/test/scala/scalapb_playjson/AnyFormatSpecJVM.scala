2a88cf9ca024ebed150df9734dfb55805f07a3ec
xuwei-k <6b656e6a69@gmail.com>
2023-12-25 10:13:58 +0900

    add `-Xsource:3`

--- core/jvm/src/test/scala/scalapb_playjson/AnyFormatSpecJVM.scala
+++ core/jvm/src/test/scala/scalapb_playjson/AnyFormatSpecJVM.scala
@@ -4,9 +4,10 @@ import com.google.protobuf.any.{Any => PBAny}
 import jsontest.anytests.AnyTest
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
+import scalapb.GeneratedMessageCompanion
 
 class AnyFormatSpecJVM extends AnyFlatSpec with Matchers with JavaAssertions {
-  override def registeredCompanions = Seq(AnyTest)
+  override def registeredCompanions: Seq[GeneratedMessageCompanion[?]] = Seq(AnyTest)
 
   "Any" should "be serialized the same as in Java (and parsed back to original)" in {
     val RawExample = AnyTest("test")
